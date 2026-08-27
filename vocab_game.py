4e20cd157c652acb5c2bf4910278e1958c869ac4
413-08-phacharapol <54260@yupparaj.ac.th>
2026-08-27 16:16:27 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -114,8 +114,6 @@ st.session_state.ans2_val = ans2
 st.session_state.ans1_val = ans3
 st.session_state.ans1_val = ans4
 
-# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
-
 
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
