d951d79ef77d00af94c0107c68bc47ea93b424a0
413-08-phacharapol <54260@yupparaj.ac.th>
2026-08-27 16:26:01 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -52,14 +52,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
       st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
   # ตรวจข้อ 3
-  if u_ans3 == "Pencil": 
+  if u_ans3 == "pencil": 
       st.success("✅ ข้อ 3: ถูกต้อง")
       score += 1
   else:
       st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
   # ตรวจข้อ 4 
-  if u_ans4 == "Banana": 
+  if u_ans4 == "banana": 
       st.success("✅ ข้อ 4: ถูกต้อง")
       score += 1
   else:
